6f43596bb0d40b5963336d5dfea05914634c0191
Marinus Klasen <marinus@mklasen.nl>
2025-09-03 19:30:57 +0000

    update webpack

--- webpack.mix.js
+++ webpack.mix.js
@@ -4,33 +4,18 @@ const mix = require('laravel-mix');
 
 mix.disableSuccessNotifications();
 
-// Configure webpack for ES6 modules and React
+// Configure webpack for ES6 modules
 mix.webpackConfig({
     resolve: {
-        extensions: ['.js', '.jsx'],
-        alias: {
-            '@wordpress/element': require.resolve('@wordpress/element'),
-            '@wordpress/i18n': require.resolve('@wordpress/i18n'),
-            '@wordpress/components': require.resolve('@wordpress/components'),
-            '@wordpress/data': require.resolve('@wordpress/data'),
-            '@wordpress/plugins': require.resolve('@wordpress/plugins'),
-        }
-    },
-    externals: {
-        '@wordpress/element': 'wp.element',
-        '@wordpress/i18n': 'wp.i18n',
-        '@wordpress/components': 'wp.components',
-        '@wordpress/data': 'wp.data',
-        '@wordpress/plugins': 'wp.plugins',
-        '@woocommerce/blocks-checkout': 'wc.blocksCheckout',
-        '@woocommerce/blocks-registry': 'wc.blocksRegistry',
+        extensions: ['.js', '.jsx']
     }
 });
 
 mix.sass('assets/src/scss/admin.scss', 'assets/dist/css/admin.css')
    .sass('assets/src/scss/main.scss', 'assets/dist/css/main.css')
    .sass('assets/src/scss/checkout.scss', 'assets/dist/css/checkout.css')
-   .sass('assets/src/scss/blocks-checkout.scss', 'assets/dist/css/blocks/checkout-pickup.css')
+   .sass('assets/src/scss/blocks-checkout.scss', 'assets/dist/css/blocks.css')
+   .sass('assets/src/scss/debug.scss', 'assets/dist/css/debug.css')
    .options({
        processCssUrls: false
    });
