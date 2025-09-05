1bb27d017845efd2e41a21354ec721ec45fadba2
Marinus Klasen <marinus@mklasen.nl>
2025-09-05 12:04:19 +0000

    move sanitization functions for shipping and pickup methods to trait

--- src/WooCommerce/Methods/Pickup.php
+++ src/WooCommerce/Methods/Pickup.php
@@ -3,11 +3,14 @@
 namespace Wuunder\Shipping\WooCommerce\Methods;
 
 use WC_Shipping_Method;
+use Wuunder\Shipping\Traits\ShippingMethodSanitization;
 
 /**
  * Wuunder Pick-up shipping method for parcel shop locator.
  */
 class Pickup extends WC_Shipping_Method {
+
+	use ShippingMethodSanitization;
 
 	/**
 	 * Available carriers for pick-up points.
@@ -110,16 +113,7 @@ class Pickup extends WC_Shipping_Method {
 				],
 				'default' => 'nl',
 				'desc_tip' => true,
-			],
-			// phpcs:ignore Squiz.PHP.CommentedOutCode.Found -- Commented out preview section for future implementation.
-			// 'preview_section' => [
-			// 'title' => __( 'Shop Locator Preview', 'wuunder-shipping' ),
-			// 'type' => 'title',
-			// 'description' => __( 'Preview of the shop locator that customers will see:', 'wuunder-shipping' ),
-			// ],
-			// 'preview' => [
-			// 'type' => 'preview_iframe',
-			// ],
+			]
 		];
 	}
 
@@ -273,49 +267,6 @@ class Pickup extends WC_Shipping_Method {
 		$this->add_rate( $rate );
 	}
 
-	/**
-	 * Sanitize the title.
-	 *
-	 * @param mixed $value Title value.
-	 * @return string Sanitized title.
-	 * @throws \Exception If the title is empty.
-	 */
-	public function sanitize_title( $value ) {
-		$value = sanitize_text_field( $value );
-		if ( empty( $value ) ) {
-			throw new \Exception( esc_html__( 'The title for a shipping method is required.', 'wuunder-shipping' ) );
-		}
-		return $value;
-	}
-
-	/**
-	 * Sanitize the cost.
-	 *
-	 * @param mixed $value Cost value.
-	 * @return string Sanitized cost.
-	 * @throws \Exception If the cost is invalid.
-	 */
-	public function sanitize_cost( $value ) {
-		$value = is_null( $value ) ? '0' : $value;
-		$value = empty( $value ) ? '0' : $value;
-		$value = wp_kses_post( trim( wp_unslash( $value ) ) );
-		$value = str_replace( array( get_woocommerce_currency_symbol(), html_entity_decode( get_woocommerce_currency_symbol() ) ), '', $value );
-
-		// Get the current locale and all possible decimal separators.
-		$locale   = localeconv();
-		$decimals = array( wc_get_price_decimal_separator(), $locale['decimal_point'], $locale['mon_decimal_point'], ',' );
-
-		// Remove whitespace, then decimals, and then invalid start/end characters.
-		$value = preg_replace( '/\s+/', '', $value );
-		$value = str_replace( $decimals, '.', $value );
-		$value = rtrim( ltrim( $value, "\t\n\r\0\x0B+*/" ), "\t\n\r\0\x0B+-*/" );
-
-		// If the value is not numeric, then throw an exception.
-		if ( ! is_numeric( $value ) ) {
-			throw new \Exception( esc_html__( 'Invalid cost entered.', 'wuunder-shipping' ) );
-		}
-		return $value;
-	}
 
 	/**
 	 * Sanitize selected carriers.
--- src/WooCommerce/Methods/Shipping.php
+++ src/WooCommerce/Methods/Shipping.php
@@ -4,11 +4,14 @@ namespace Wuunder\Shipping\WooCommerce\Methods;
 
 use WC_Shipping_Method;
 use Wuunder\Shipping\Models\Carrier;
+use Wuunder\Shipping\Traits\ShippingMethodSanitization;
 
 /**
  * Simple Wuunder shipping method that uses instance settings.
  */
 class Shipping extends WC_Shipping_Method {
+
+	use ShippingMethodSanitization;
 
 	/**
 	 * Wuunder carrier data.
@@ -132,20 +135,6 @@ class Shipping extends WC_Shipping_Method {
 		$this->add_rate( $rate );
 	}
 
-	/**
-	 * Sanitize the title by checking if the title is empty.
-	 *
-	 * @param mixed $value Title value.
-	 * @return string Sanitized title.
-	 * @throws \Exception If the title is empty.
-	 */
-	public function sanitize_title( $value ) {
-		$value = sanitize_text_field( $value );
-		if ( empty( $value ) ) {
-			throw new \Exception( esc_html__( 'The title for a shipping method is required.', 'wuunder-shipping' ) );
-		}
-		return $value;
-	}
 
 	/**
 	 * Sanitize the carrier by checking if the carrier value exists in the database.
@@ -162,32 +151,4 @@ class Shipping extends WC_Shipping_Method {
 		return $value;
 	}
 
-	/**
-	 * Sanitize the cost, inspired by the WooCommerce shipping method sanitize_cost function.
-	 *
-	 * @param mixed $value Cost value.
-	 * @return string Sanitized cost.
-	 * @throws \Exception If the cost is invalid.
-	 */
-	public function sanitize_cost( $value ) {
-		$value = is_null( $value ) ? '0' : $value;
-		$value = empty( $value ) ? '0' : $value;
-		$value = wp_kses_post( trim( wp_unslash( $value ) ) );
-		$value = str_replace( array( get_woocommerce_currency_symbol(), html_entity_decode( get_woocommerce_currency_symbol() ) ), '', $value );
-
-		// Get the current locale and all possible decimal separators.
-		$locale   = localeconv();
-		$decimals = array( wc_get_price_decimal_separator(), $locale['decimal_point'], $locale['mon_decimal_point'], ',' );
-
-		// Remove whitespace, then decimals, and then invalid start/end characters.
-		$value = preg_replace( '/\s+/', '', $value );
-		$value = str_replace( $decimals, '.', $value );
-		$value = rtrim( ltrim( $value, "\t\n\r\0\x0B+*/" ), "\t\n\r\0\x0B+-*/" );
-
-		// If the value is not numeric, then throw an exception.
-		if ( ! is_numeric( $value ) ) {
-			throw new \Exception( esc_html__( 'Invalid cost entered.', 'wuunder-shipping' ) );
-		}
-		return $value;
-	}
 }
